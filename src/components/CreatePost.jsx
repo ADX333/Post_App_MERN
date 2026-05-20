aafba63190933f18d6c1a22f696d3200fc05ca3a
Anshuman <anshumandixit333@gmail.com>
2026-05-20 23:01:07 +0530

    frontend editing

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -7,7 +7,7 @@ function CreatePost() {
       <h1 className="mb-10 font-extrabold text-3xl">Create Post</h1>
       <form className='flex flex-col'>
         <input className="bg-gray-700 border-2 border-amber-300 hover:scale-103 transition-all duration-300 rounded h-50 w-100 mb-2 cursor-pointer" type="file" placeholder='Choose an image' name="image" accept="image/*"></input>
-        <input className="bg-gray-700 border-2 border-amber-300 hover:scale-103 transition-all duration-300 rounded w-100 mb-2" type="text" name="caption" required placeholder='Caption'/>
+        <input className="bg-gray-700 border-2 border-amber-300 p-1 hover:scale-103 transition-all duration-300 rounded w-100 mb-2" type="text" name="caption" required placeholder='Caption'/>
         <button type="submit"></button>
 
       </form>
